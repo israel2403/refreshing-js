68b8aa11c942ccfa68f432e9c21f962515f05f42
Israel Huerta Fer <israelhf24@gmail.com>
2025-10-30 13:46:30 -0600

    arrow fn

--- play.js
+++ play.js
@@ -4,8 +4,21 @@ const hasHobbies = true;
 
 age = age + 1;
 
-function summarizeUser(userName, userAge, userHasHobbies){
+const summarizeUser = (userName, userAge, userHasHobbies)=>{
   return ('Name is '+ userName + ', Age is '+ userAge + ' and he/she has hobbies: '+ userHasHobbies);
 }
+
+const add = (a, b) => a+b;
+console.log(add(1,2));
+
+// if you have an arrow fn with one argument you can remove the '()' from the parameter list
+const addOne = a => a+1;
+console.log(addOne(1));
+
+// if you have an arrow fn with no arguments you must put in empty parenthesis
+const addRandom = () => 20;
+console.log(addRandom());
+
+
 
 console.log(summarizeUser(name, age, hasHobbies));
