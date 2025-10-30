bb415e90916d2f74c675e5cff3be19a82dcca9e2
Israel Huerta Fer <israelhf24@gmail.com>
2025-10-30 13:27:05 -0600

    first commit

---
+++ play.js
@@ -0,0 +1,11 @@
+var name = "isra";
+
+var age = 20;
+var hasHobbies = true;
+
+
+function summarizeUser(userName, userAge, userHasHobbies){
+  return ('Name is '+ userName + ', Age is '+ userAge + ' and he/she has hobbies: '+ userHasHobbies);
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
